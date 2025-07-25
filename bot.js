0d5a390cf8f6562ea5b64301b9dff9967e5fbbda
Leon Stevans <leonstevansx@gmail.com>
2025-07-24 21:36:40 -0300

    remove dot from welcome message

--- bot.js
+++ bot.js
@@ -29,7 +29,7 @@ client.on('guildMemberAdd', async (member) => {
     const channel = member.guild.channels.cache.get(WELCOME_CHANNEL_ID);
     if (!channel) return;
 
-    const welcomeMessage = `Welcome to Tempo, ${member}.`;
+    const welcomeMessage = `Welcome to Tempo, ${member}`;
     const welcomeGif = './assets/gif/Welcome.gif';
 
     await channel.send({ content: `${welcomeMessage}\n${welcomeGif}` });
